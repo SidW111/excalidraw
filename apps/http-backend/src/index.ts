23cb7b805b3a36f36d03d9d6c78aa33761ea1ca6
SidW111 <siddhantwainngade@gmail.com>
2025-08-25 12:06:48 +0530

    added common and backend-common in packages

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,16 +1,20 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 import { userMiddleware } from "./middleware";
 
 const app = express();
 
 app.post("/signup", async (req, res) => {});
 app.post("/signin", async (req, res) => {
-    const userId = 1;
-    const token = jwt.sign({userId},JWT_SECRET)
+  const userId = 1;
+  const token = jwt.sign({ userId }, JWT_SECRET);
 });
-app.post("/room" , userMiddleware, async (req, res) => {});
+app.post("/room", userMiddleware, async (req, res) => {
+  res.json({
+    roomId: 123,
+  });
+});
 
 app.listen(3001, () => {
   console.log({ msg: "connected http" });
