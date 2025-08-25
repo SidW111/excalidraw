a2934770351e07ff2a6a1c6337645c298396ae81
SidW111 <siddhantwainngade@gmail.com>
2025-08-25 10:34:42 +0530

    initialized http-backend added middleware

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { userMiddleware } from "./middleware";
+
+const app = express();
+
+app.post("/signup", async (req, res) => {});
+app.post("/signin", async (req, res) => {
+    const userId = 1;
+    const token = jwt.sign({userId},JWT_SECRET)
+});
+app.post("/room" , userMiddleware, async (req, res) => {});
+
+app.listen(3001, () => {
+  console.log({ msg: "connected http" });
+});
---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,25 @@
+import { Response, Request, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+
+export function userMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const token = req.headers["authorization"] ?? "";
+  if (!token) {
+    return res.json({ msg: "token not found" });
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, JWT_SECRET);
+    if (decodedToken) {
+      //@ts-ignore
+      decodedToken.userId = req.userId;
+      next();
+    }
+  } catch (error) {
+    res.json({ message: "User is unauthorized" });
+  }
+}
