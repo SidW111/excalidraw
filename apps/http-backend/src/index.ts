d3c500786e8270934a67a8585000216203c9a1b4
SidW111 <siddhantwainngade@gmail.com>
2025-08-26 16:50:29 +0530

    added sign up functionality

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,38 +2,61 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { userMiddleware } from "./middleware";
-import {CreateUserSchema,SignInSchema,CreateRoomSchema} from "@repo/common/types"
+import {
+  CreateUserSchema,
+  SignInSchema,
+  CreateRoomSchema,
+} from "@repo/common/types";
+import { prismaClient } from "@repo/db/client";
 
 const app = express();
 
 app.post("/signup", async (req, res) => {
-  const data = CreateUserSchema.safeParse(req.body)
-  if(!data.success){
-    res.json({
-      message:"incorrect input"
-    })
-    return;
+  try {
+    const parsedData = CreateUserSchema.safeParse(req.body);
+
+    if (!parsedData.success) {
+      res.json({
+        message: "incorrect input",
+      });
+      return;
+    }
+
+    const result = await prismaClient.user.create({
+      data: {
+        email: parsedData.data.username,
+        password: parsedData.data.password,
+        name: parsedData.data.name,
+      },
+    });
+
+    if (result) {
+      res.json({ message: "User created successfully" });
+    }
+  } catch (error) {
+    res.status(404).json({
+      message: "create user failed",
+    });
   }
-
 });
-app.post("/signin", async (req, res) => {
 
-  const data = SignInSchema.safeParse(req.body)
-  if(!data.success){
+app.post("/signin", async (req, res) => {
+  const data = SignInSchema.safeParse(req.body);
+  if (!data.success) {
     res.json({
-      message:"incorrect input"
-    })
+      message: "incorrect input",
+    });
     return;
   }
   const userId = 1;
   const token = jwt.sign({ userId }, JWT_SECRET);
 });
 app.post("/room", userMiddleware, async (req, res) => {
-  const data = CreateRoomSchema.safeParse(req.body)
-  if(!data.success){
+  const data = CreateRoomSchema.safeParse(req.body);
+  if (!data.success) {
     res.json({
-      message:"incorrect input"
-    })
+      message: "incorrect input",
+    });
     return;
   }
   res.json({
