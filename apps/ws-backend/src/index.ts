9e3fcfe6d23844f666f1d0937d3d417ba300bbc0
SidW111 <siddhantwainngade@gmail.com>
2025-08-27 16:53:10 +0530

    added checkUser function in ws-backend

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -4,6 +4,18 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 
 const wss = new WebSocketServer({ port: 8080 });
 
+function userCheck(token: string): string | null{
+  const decodedToken = jwt.verify(token, JWT_SECRET);
+  if(typeof decodedToken == "string"){
+    return null;
+  }
+  if(!decodedToken || !decodedToken.userId){
+    return null;
+  }
+
+  return decodedToken.userId;
+}
+
 wss.on("connection", function connection(ws, request) {
   const url = request.url;
   if (!url) {
@@ -12,15 +24,13 @@ wss.on("connection", function connection(ws, request) {
 
   const queryParams = new URLSearchParams(url.split("?")[1]);
   const token = queryParams.get("token") || "";
-  const decodedToken = jwt.verify(token, JWT_SECRET);
-  if(typeof decodedToken == "string"){
-    ws.close();
-    return
-  }
-  if(!decodedToken || !decodedToken.userId){
+  const userId = userCheck(token);
+
+  if(!userId){
     ws.close()
-    return ;
+    return;
   }
+  
   ws.on("message", function message(data) {
     ws.send("pong");
   });
