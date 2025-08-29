207fb4363df43e2249aeabb64a90be1b6779d3ce
SidW111 <siddhantwainngade@gmail.com>
2025-08-29 10:15:18 +0530

    added canvas looks fun

--- apps/excalidraw-frontend/app/page.tsx
+++ apps/excalidraw-frontend/app/page.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div >
-      
-      Excalidraw frontend
+    <div className="w-screen h-screen flex items-center justify-center gap-3">
+      <Link href="/signup">
+        <button className="px-6 py-3 rounded bg-blue-500 hover:bg-blue-600 text-white">Sign Up</button>
+      </Link>
+      <Link href="/signin">
+        <button className="px-6 py-3 rounded bg-blue-500 hover:bg-blue-600 text-white">Sign In</button>
+      </Link>
     </div>
   );
 }
