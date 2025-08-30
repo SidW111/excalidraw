fe3e6f77838f3997e54e2709e1f29f7cafd63383
SidW111 <siddhantwainngade@gmail.com>
2025-08-30 09:37:35 +0530

    added topbar

--- apps/excalidraw-frontend/components/Canvas.tsx
+++ apps/excalidraw-frontend/components/Canvas.tsx
@@ -1,7 +1,13 @@
 import { initDraw } from "@/draw";
 import { useEffect, useRef, useState } from "react";
 
-export default function Canvas({ roomId ,socket}: { roomId: string,socket:WebSocket }) {
+export default function Canvas({
+  roomId,
+  socket,
+}: {
+  roomId: string;
+  socket: WebSocket;
+}) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
@@ -13,11 +19,25 @@ export default function Canvas({ roomId ,socket}: { roomId: string,socket:WebSoc
 
   return (
     <div className="">
+      <div className="fixed  top-5 left-100 flex  items-center justify-center gap-2  text-white bg-black p-2 ">
+        <button className="px-6 py-3 bg-sky-500 rounded-2xl cursor-pointer">
+          circle
+        </button>
+        <button className="px-6 py-3 bg-sky-500 rounded-2xl cursor-pointer">
+          line
+        </button>
+        <button className="px-6 py-3 bg-sky-500 rounded-2xl cursor-pointer">
+          rectangle
+        </button>
+        <button className="px-6 py-3 bg-sky-500 rounded-2xl cursor-pointer">
+          none
+        </button>
+      </div>
       <canvas
         ref={canvasRef}
         className="bg-white"
-        height={2000}
-        width={2000}
+        height={window.innerHeight}
+        width={window.innerWidth}
       ></canvas>
     </div>
   );
